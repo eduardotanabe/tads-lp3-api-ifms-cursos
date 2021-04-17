63a107d237378f1cfe8acf23c60785b13123e35a
Eduardo Tanabe <e_tanabe@yahoo.com.br>
2021-04-17 16:49:13 -0400

    Implementação do ícone de carregamento

--- app/index.js
+++ app/index.js
@@ -1,24 +1,31 @@
 const apiData = document.querySelector(".cursos")
 const listaSuspensa = document.querySelector("#listaSuspensa")
+const spinner = document.querySelector('.spinner-border')
+
+spinner.style.display = "none"
 
 async function listarCursos() {
     const url = "http://localhost:3000/cursosIFMS"
+    spinner.style.display = "block"
     const response = await axios.get(url)
+    spinner.style.display = "none"
     const cursos = response.data
-
+    
     cursos.forEach(curso => {
         apiData.innerHTML += `
             <h2>${curso.curso}</h2>
             <p>Nível de ensino: ${curso.nivelDeEnsino} </p>
             <p>Duração: ${curso.duracao}</p>
             <p>Município: ${curso.municipio}</p>
-        `
-    });
-}
+            `
+        });
+    }
 
-async function getNivelDeEnsino() {
+    async function getNivelDeEnsino() {
     const url = 'http://localhost:3000/nivelDeEnsino'
+    spinner.style.display = "block"
     const response = await axios.get(url)
+    spinner.style.display = "none"
     const nivelDeEnsino = Array.from(response.data)
 
     nivelDeEnsino.forEach(nivel => {
@@ -33,8 +40,9 @@ async function search(query) {
     }
 
     const url = `http://localhost:3000/cursosIFMS?q=${query}`
-
+    spinner.style.display = "block"
     const response = await axios.get(url)
+    spinner.style.display = "none"
     const cursos = Array.from(response.data)
 
     apiData.innerHTML = ""
